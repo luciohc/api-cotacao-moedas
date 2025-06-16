f0b71c70567b0192438eb6962ec840cfdbd8b931
luciohc <lucio@gmail.com>
2025-06-16 14:09:16 -0300

    Corrigindo duplo app.listen

--- backend/app.js
+++ backend/app.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
@@ -129,3 +130,5 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Servidor Backend rodando na porta ${PORT}`);
 });
+
+
