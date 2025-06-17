738e62181b5a2aeb58562a4f900d8d2129b2f28f
luciohc <lucio@gmail.com>
2025-06-16 22:41:18 -0300

    Fix: adicionando conexão com PostgreSQL Railway

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,11 +1,9 @@
 const { Pool } = require('pg');
+require('dotenv').config();
 
 const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'cotacao_db',
-  password: 'Al171178@',
-  port: 5432,
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }
 });
 
 module.exports = pool;
